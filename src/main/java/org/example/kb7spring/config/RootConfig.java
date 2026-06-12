8463f4cbd0f66da6e5cd9df47ff321d632dfd54a
Tetz <xenosign@naver.com>
2026-06-12 09:46:18 +0900

    260612

---
+++ src/main/java/org/example/kb7spring/config/RootConfig.java
@@ -0,0 +1,7 @@
+package org.example.kb7spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+}
---
+++ src/main/java/org/example/kb7spring/config/ServletConfig.java
@@ -0,0 +1,30 @@
+package org.example.kb7spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = {"org.example.kb7spring.controller"})
+public class ServletConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
+
+    // jsp view resolver 설정
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry){
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/views/");
+        bean.setSuffix(".jsp");
+        registry.viewResolver(bean);
+    }
+}
