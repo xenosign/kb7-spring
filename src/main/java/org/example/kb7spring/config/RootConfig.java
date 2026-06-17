cc08ca4a1a3be057fa1c5aeaf0f5a5d58b8a1101
Tetz <xenosign@naver.com>
2026-06-17 10:11:29 +0900

    260617

--- src/main/java/org/example/kb7spring/config/RootConfig.java
+++ src/main/java/org/example/kb7spring/config/RootConfig.java
@@ -1,16 +1,62 @@
 package org.example.kb7spring.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.sql.DataSource;
+
 @Configuration
+@PropertySource({"classpath:/application.properties"})
+@MapperScan(basePackages = {"org.example.kb7spring"})
 @ComponentScan(basePackages = {"org.example.kb7spring"},
         excludeFilters = @ComponentScan.Filter(
                 type = FilterType.ANNOTATION,
                 classes = Controller.class
         )
 )
-public class RootConfig {}
+public class RootConfig {
+    @Value("${jdbc.driver}") String driver;
+    @Value("${jdbc.url}") String url;
+    @Value("${jdbc.username}") String username;
+    @Value("${jdbc.password}") String password;
+
+        @Bean
+        public DataSource dataSource() {
+                HikariConfig config = new HikariConfig();
+                config.setDriverClassName(driver);
+                config.setJdbcUrl(url);
+                config.setUsername(username);
+                config.setPassword(password);
+                HikariDataSource dataSource = new HikariDataSource(config);
+                return dataSource;
+        }
+
+        @Autowired
+        ApplicationContext applicationContext;
+
+        @Bean
+        public SqlSessionFactory sqlSessionFactory() throws Exception {
+                SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+                sqlSessionFactory.setConfigLocation(
+                        applicationContext.getResource("classpath:/mybatis-config.xml"));
+                sqlSessionFactory.setDataSource(dataSource());
+                return (SqlSessionFactory) sqlSessionFactory.getObject();
+        }
+
+        @Bean
+        public DataSourceTransactionManager transactionManager(){
+                DataSourceTransactionManager manager = new DataSourceTransactionManager(dataSource());
+                return manager;
+        }
+
+}
--- src/main/java/org/example/kb7spring/member/repository/MemberRepositoryV1.java
+++ src/main/java/org/example/kb7spring/member/repository/MemberRepositoryV1.java
@@ -8,13 +8,21 @@ import java.util.List;
 
 @Repository
 public class MemberRepositoryV1 {
-    public List<Member> findAll() {
-        List<Member> memberList = new ArrayList<>();
+    private final List<Member> memberList = new ArrayList<>();
 
+    public MemberRepositoryV1() {
         memberList.add(new Member(1L, "ronaldo@example.com", "1985. 02. 05","호날두", "플래티넘", 300000000L));
         memberList.add(new Member(2L, "sjk@example.com", "1985. 09. 19", "송중기", "골드", 3000000L));
         memberList.add(new Member(3L, "xenosign@example.com", "1985. 11. 18", "이효석", "아이언", 10L));
 
+    }
+
+    public List<Member> findAll() {
         return memberList;
+    }
+
+    public void save(Member member) {
+        member.setId((long) (memberList.size() + 1));
+        memberList.add(member);
     }
 }
