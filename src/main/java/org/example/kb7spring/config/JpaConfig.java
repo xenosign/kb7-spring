419d5dccbdb60ce37e717fe47ac7e2a204c55cc2
Tetz <xenosign@naver.com>
2026-06-22 10:04:13 +0900

    260622

--- src/main/java/org/example/kb7spring/config/JpaConfig.java
+++ src/main/java/org/example/kb7spring/config/JpaConfig.java
@@ -5,6 +5,7 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -20,44 +21,55 @@ import java.util.Properties;
         "org.example.kb7spring.member.repository",
         "org.example.kb7spring.student.repository"
 })
+@PropertySource("classpath:application.properties")
 public class JpaConfig {
-
-    @Value("${jdbc.driver}") String driver;
-    @Value("${jdbc.url}") String url;
-    @Value("${jdbc.username}") String username;
-    @Value("${jdbc.password}") String password;
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    private String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+    @Value("${hibernate.dialect}")
+    private String dialect;
+    @Value("${hibernate.hbm2ddl.auto}")
+    private String ddlAuto;
+    @Value("${hibernate.show_sql}")
+    private boolean showSql;
+    @Value("${hibernate.format_sql}")
+    private boolean formatSql;
 
     @Bean
-    public DataSource jpaDataSource() {
+    public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
-        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        config.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/kb7spring");
-        config.setUsername("root");
-        config.setPassword("1234");
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
         return new HikariDataSource(config);
     }
 
-    // JPA EntityManagerFactory
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
-        emf.setDataSource(jpaDataSource());
-        emf.setPackagesToScan("org.example.kb7spring");
-
+        LocalContainerEntityManagerFactoryBean emf =
+                new LocalContainerEntityManagerFactoryBean();
+        emf.setDataSource(dataSource()); // DB 연결 방법 전달
+        emf.setPackagesToScan("org.example.kb7spring"); // Entity 를 읽을 패키지 설정
         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-        adapter.setShowSql(true);
+        adapter.setShowSql(showSql); // JPA 실행 시, SQL 쿼리를 보여줄지 옵션
         emf.setJpaVendorAdapter(adapter);
 
         Properties props = new Properties();
-        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-        props.setProperty("hibernate.hbm2ddl.auto", "update");
-        props.setProperty("hibernate.format_sql", "true");
+        props.setProperty("hibernate.dialect", dialect);
+        props.setProperty("hibernate.hbm2ddl.auto", ddlAuto);
+        props.setProperty("hibernate.show_sql", String.valueOf(showSql));
+        props.setProperty("hibernate.format_sql", String.valueOf(formatSql));
         emf.setJpaProperties(props);
 
         return emf;
     }
 
-    // JPA 트랜잭션 매니저
     @Bean
     public JpaTransactionManager transactionManager() {
         return new JpaTransactionManager(entityManagerFactory().getObject());
