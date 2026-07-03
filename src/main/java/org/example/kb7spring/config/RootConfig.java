08451ed0be081165072c40d7a87f19d07a3d9084
Tetz <xenosign@naver.com>
2026-07-03 14:33:18 +0900

    Update RootConfig.java

--- src/main/java/org/example/kb7spring/config/RootConfig.java
+++ src/main/java/org/example/kb7spring/config/RootConfig.java
@@ -38,7 +38,7 @@ public class RootConfig {
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        config.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/kb7spring?rewriteBatchedStatements=true");
+        config.setJdbcUrl(url);
         config.setUsername("root");
         config.setPassword("1234");
         return new HikariDataSource(config);
